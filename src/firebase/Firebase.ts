94db3b88e5c31322b453b446552b72ef5e54c321
Dana Basken <dbasken@gmail.com>
2024-08-03 14:44:40 -0400

    make sure Firebase is initialized

--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -3,7 +3,10 @@
  */
 
 import * as admin from "firebase-admin";
+import log4js from "log4js";
 import {Config} from "../config/Config";
+
+const logger = log4js.getLogger("Firebase");
 
 export class Firebase {
 
@@ -12,21 +15,29 @@ export class Firebase {
   private static _firestore: admin.firestore.Firestore;
 
   static get app() {
-    if (!Firebase._app) {
-      const config = Config.get("firestore");
-      Firebase._app = admin.initializeApp(config ? {credential: admin.credential.cert(config)} : undefined);
-      Firebase._auth = Firebase.app.auth();
-      Firebase._firestore = Firebase.app.firestore();
-    }
+    Firebase._ensureInitialized();
     return Firebase._app;
   }
 
   static get auth(): admin.auth.Auth {
+    Firebase._ensureInitialized();
     return Firebase._auth;
   }
 
   static get firestore(): admin.firestore.Firestore {
+    Firebase._ensureInitialized();
     return Firebase._firestore;
+  }
+
+  private static _ensureInitialized(): void {
+    if (!Firebase._app) {
+      logger.trace("initializing Firebase...");
+      const config = Config.get("firestore");
+      Firebase._app = admin.initializeApp(config ? {credential: admin.credential.cert(config)} : undefined);
+      Firebase._auth = Firebase.app.auth();
+      Firebase._firestore = Firebase.app.firestore();
+      logger.trace("Firebase initialized.");
+    }
   }
 
 }
