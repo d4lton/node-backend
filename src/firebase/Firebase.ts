aae65192c9dda7aec38100028744524658b23c1d
Dana Basken <dbasken@gmail.com>
2024-08-03 14:35:27 -0400

    exposed Firebase.firestore

--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -9,20 +9,24 @@ export class Firebase {
 
   private static _app?: admin.app.App;
   private static _auth?: admin.auth.Auth;
+  private static _firestore: admin.firestore.Firestore;
 
   static get app() {
     if (!Firebase._app) {
       const config = Config.get("firestore");
-      Firebase._app = admin.initializeApp({credential: admin.credential.cert(config)});
+      Firebase._app = admin.initializeApp(config ? {credential: admin.credential.cert(config)} : undefined);
+      Firebase._auth = Firebase.app.auth();
+      Firebase._firestore = Firebase.app.firestore();
     }
     return Firebase._app;
   }
 
   static get auth(): admin.auth.Auth {
-    if (!Firebase._auth) {
-      Firebase._auth = Firebase.app.auth();
-    }
     return Firebase._auth;
+  }
+
+  static get firestore(): admin.firestore.Firestore {
+    return Firebase._firestore;
   }
 
 }
